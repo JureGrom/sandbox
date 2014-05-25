084699a4189be1ed05ada9a75b456fd6d02e0f17
Jure Grom <juregrom@gmail.com>
2014-05-26 00:12:49 +0200

    impl parsing pn

--- www/msisdnParser.php
+++ www/msisdnParser.php
@@ -10,6 +10,7 @@ namespace jureg;
 
 require 'vendor/autoload.php';
 
+use Phonenumbers\PhoneNumberType;
 use Phonenumbers\PhoneNumberUtil;
 
 /*
@@ -20,7 +21,7 @@ class ParsingResult {
     public $mnoIdentifier = "si.mobil";
     public $cntDiallingCode;
     public $subscriber;
-    public $cntIsoCode = "SI";
+    public $cntIsoCode;
 
     function __construct($msisdn)
     {
@@ -29,9 +30,51 @@ class ParsingResult {
 
         $this->cntDiallingCode = $parsedPN->getCountryCode();
         $this->subscriber = $parsedPN->getNationalNumber();
+        $this->cntIsoCode = $phoneUtil->getRegionCodeForNumber($parsedPN);
+        //todo use giggsey PhoneNumberToCarrierMapper
+        $type = $phoneUtil->getNumberType($parsedPN);
+        $this->setMnoIdentifier($type);
     }
 
-
+    private function setMnoIdentifier($type)
+    {
+        switch($type)
+        {
+            case PhoneNumberType::FIXED_LINE:
+                $this->mnoIdentifier = "FIXED_LINE";
+                break;
+            case PhoneNumberType::FIXED_LINE_OR_MOBILE:
+                $this->mnoIdentifier = "FIXED_LINE_OR_MOBILE";
+                break;
+            case PhoneNumberType::MOBILE:
+                $this->mnoIdentifier = "MOBILE";
+                break;
+            case PhoneNumberType::PAGER:
+                $this->mnoIdentifier = "PAGER";
+                break;
+            case PhoneNumberType::PERSONAL_NUMBER:
+                $this->mnoIdentifier = "PERSONAL_NUMBER";
+                break;
+            case PhoneNumberType::PREMIUM_RATE:
+                $this->mnoIdentifier = "PREMIUM_RATE";
+                break;
+            case PhoneNumberType::TOLL_FREE:
+                $this->mnoIdentifier = "TOLL_FREE";
+                break;
+            case PhoneNumberType::SHARED_COST:
+                $this->mnoIdentifier = "SHARED_COST";
+                break;
+            case PhoneNumberType::UAN:
+                $this->mnoIdentifier = "UAN";
+                break;
+            case PhoneNumberType::UNKNOWN:
+                $this->mnoIdentifier = "UNKNOWN";
+                break;
+            case PhoneNumberType::VOIP:
+                $this->mnoIdentifier = "VOIP";
+                break;
+        }
+    }
 }
 
 echo json_encode(new ParsingResult("+38651364137"));
