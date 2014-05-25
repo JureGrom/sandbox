05e008c81fefc51d83404bf02beac93b15781be2
Jure Grom <juregrom@gmail.com>
2014-05-25 23:33:58 +0200

    added test stub

---
+++ tests/ParsingResultTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juregrom
+ * Date: 25/05/14
+ * Time: 22:50
+ */
+
+namespace jureg;
+
+
+class ParsingResultTest extends \PHPUnit_Framework_TestCase {
+
+    public function testParsing()
+    {
+        $t = new ParsingResult("+38651234567");
+
+        $this->assertEquals(386,$t->cntDiallingCode);
+        $this->assertEquals("SI",$t->cntIsoCode);
+        $this->assertEquals(51234567,$t->subscriber);
+        $this->assertEquals("mobitel",$t->mnoIdentifier);
+    }
+
+
+}
+ 
